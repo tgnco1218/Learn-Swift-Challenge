ad20b81e102c0dab43eeb4670afc52e546c761f2
Yi-Ting Kuo <tgnco1218@yahoo.com.tw>
2019-12-31 15:46:55 +0800

    Add files via upload

---
+++ LearnSwiftChallenge/LearnSwiftChallenge1.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//
+// CodeWithChris Learn Swift for Beginners
+// http://codewithchris.com/learn-swift
+//
+// Challenge #1: The Lost Animal Challenge
+//
+// Instructions: 
+// Given the two arrays below, write a function that takes a String as an input parameter and returns a Boolean value. The function should return true if the String input is in either array and it should return false if the String input is in neither array.
+//
+// Examples:
+// Call your function and pass in the String "cat" as the input. Your function should return true
+// Call your function and pass in the String "cow" as the input. Your function should return false
+ 
+let array1 = ["dog", "cat", "bird", "pig"]
+let array2 = ["turtle", "snake", "lizard", "shark"]
+
+// Write your function below:
+
+func seeIfInArray(input:String)->Bool {
+    if array1.contains(input) || array2.contains(input) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+seeIfInArray(input: "cat")
+seeIfInArray(input: "cow")
